e9b51bd4bb9aaaa6d353de274b95c0392e897f78
Victor Vo <79146455+victorkvo@users.noreply.github.com>
2021-05-17 14:41:29 -0500

    Initial commit

---
+++ src/test/java/com/tts4/hw/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts4.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
